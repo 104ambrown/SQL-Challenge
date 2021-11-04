d42618160a701acbff53aba98213d27dccf2f9e8
Zas Mama <104ambrown@gmail.com>
2021-11-03 23:36:55 -0400

    Created a ERD on quickdatabasediagrams.com and saved the diagram as a .png

---
+++ EmployeeSQL/SQL-Challenge.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Departments('dept_no', 'dept_name')
+	
+
+CREATE TABLE Dept_Employee('emp_no', 'dept_no')
+
+CREATE TABLE Dept_Manager('emp_no', 'dept_no')
+
+CREATE TABLE Employees('emp_no', 'emp_title', 'first_name', 'last_name', 'sex', 'hire_date')
+
+CREATE TABLE Salaries('emp_no', 'salary')
+
+CREATE TABLE Titles('title_id', 'title')
+
+title_id = emp_title
